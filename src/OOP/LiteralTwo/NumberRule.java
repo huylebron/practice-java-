4bdd219af75c9b2c8f1a420a642054b7714f6374
huylebron <doquochuy27102003@gmail.com>
2025-10-16 15:12:55 +0700

    bunus rule number

---
+++ src/OOP/LiteralTwo/NumberRule.java
@@ -0,0 +1,10 @@
+﻿package OOP.LiteralTwo;
+
+public class NumberRule implements  ValidationRule {
+
+
+    @Override
+    public boolean isValid(String password) {
+        return password != null && password.matches(".*\\\\d.*");
+    }
+}
