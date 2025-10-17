52518376d773d2ca78a1bc0b64092f11e18d8b62
huylebron <doquochuy27102003@gmail.com>
2025-10-17 14:46:19 +0700

    1221. Split a String in Balanced Strings

---
+++ src/LeetCode/Easy/balancedStringSplit.java
@@ -0,0 +1,23 @@
+﻿package LeetCode.Easy;
+
+public class balancedStringSplit {
+
+    public int balancedStringSplitImp ( String s )  {
+
+        int balance  = 0 ;
+        int count = 0 ;
+
+        for ( char c : s.toCharArray()) {
+            if ( c == 'L') {
+                balance++;
+            }
+            else {
+                balance--;
+            }
+            if ( balance == 0 ) {
+                count ++ ;
+            }
+        }
+        return count;
+    }
+}
